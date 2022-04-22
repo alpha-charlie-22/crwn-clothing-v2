1c0946bb07fea52d494478f43f0251e7c33cd3a0
ace-1618 <a.medi63@gmail.com>
2022-04-22 12:48:30 -0500

    add category preview, add shop subroutes, cleanup

---
+++ src/context/categories.context.js
@@ -0,0 +1,30 @@
+import { createContext, useState, useEffect } from 'react';
+import { getCategoriesAndDocuments } from '../utils/firebase/firebase.utils';
+
+// import SHOP_DATA from '../shop-data';
+
+// actual value to access
+export const CategoriesContext = createContext({
+  categoriesMap: {}
+});
+
+export const CategoriesProvider = ({ children }) => {
+  const [categoriesMap, setCategoriesMap] = useState({});
+
+  useEffect(() => {
+    const getCategoriesMap = async () => {
+      const categoryMap = await getCategoriesAndDocuments('categories');
+      console.log({categoryMap});
+      setCategoriesMap(categoryMap);
+    }
+    getCategoriesMap();
+  }, []);
+
+  // ONE-TIME DATALOAD ONLY
+  // useEffect(() => {
+  //   addCollectionAndDocuments('categories', SHOP_DATA)
+  // }, []);
+
+  const value = { categoriesMap };
+  return <CategoriesContext.Provider value={value}>{children}</CategoriesContext.Provider>
+}
