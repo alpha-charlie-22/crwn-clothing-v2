b8cc25fd5c0d8dc10485b9debf05e761879e9240
ace-1618 <a.medi63@gmail.com>
2022-05-10 20:48:46 -0500

    migrate categories fetch from thunk to sagas

--- src/store/store.js
+++ src/store/store.js
@@ -3,6 +3,8 @@ import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
+import { rootSaga } from './rootSaga';
 
 import { rootReducer } from './rootReducer';
 
@@ -12,14 +14,18 @@ const persistConfig = {
   whitelist: ['cart']
 };
 
+const sagaMiddleware = createSagaMiddleware();
+
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middleWare = [process.env.NODE_ENV !== 'production' && logger, thunk].filter(Boolean);
+const middleWare = [process.env.NODE_ENV !== 'production' && logger, sagaMiddleware].filter(Boolean);
 
 const composeEnhancer = (process.env.NODE_ENV !== 'production' && window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose 
 
 const composedEnhancers = composeEnhancer(applyMiddleware(...middleWare));
 
 export const store = createStore(persistedReducer, undefined, composedEnhancers);
+
+sagaMiddleware.run(rootSaga);
 
 export const persistor = persistStore(store);
